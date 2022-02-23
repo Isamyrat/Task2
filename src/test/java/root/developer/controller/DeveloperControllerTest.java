e6b375c3626fa6d8241bcabc3439e9621dd0f0e6
Isamyrat <kurbanowisa07@gmail.com>
2022-02-23 07:13:44 +0300

    added to tests jwt

--- src/test/java/root/developer/controller/DeveloperControllerTest.java
+++ src/test/java/root/developer/controller/DeveloperControllerTest.java
@@ -4,18 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import root.developer.model.Developer;
+import root.developer.security.model.JwtRequest;
+import root.developer.security.model.JwtResponse;
+import root.developer.security.model.Role;
+import root.developer.security.service.AuthService;
 import root.developer.service.DeveloperService;
 
 import java.util.*;
@@ -25,8 +27,8 @@ import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(MockitoJUnitRunner.class)
-@WebMvcTest(DeveloperController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class DeveloperControllerTest {
 
     @Autowired
@@ -38,8 +40,8 @@ class DeveloperControllerTest {
     @MockBean
     private DeveloperService developerService;
 
-    @InjectMocks
-    private DeveloperController developerController;
+    @Autowired
+    private AuthService authService;
 
     Developer RECORD_1 = new Developer(1L, "Nikita", "ganu@gmail.com");
     Developer RECORD_2 = new Developer(2L, "Misha", "orlov@gmail.com");
@@ -47,14 +49,22 @@ class DeveloperControllerTest {
 
     @Test
     public void getAllRecords_success() throws Exception {
+        JwtRequest jwtRequest = new JwtRequest();
+        jwtRequest.setIssuer("GP");
+        jwtRequest.setRoles(Collections.singleton(Role.ADMIN));
+
+        JwtResponse jwtResponse = authService.login(jwtRequest);
+
         List<Developer> developerList = new ArrayList<>(Arrays.asList(RECORD_1, RECORD_2, RECORD_3));
+        when(developerService.findAll()).thenReturn(developerList);
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/developer/findAll")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + jwtResponse.getAccessToken())
+                .with(SecurityMockMvcRequestPostProcessors.user("task2").roles("ADMIN"));
 
-        Mockito.when(developerService.findAll()).thenReturn(developerList);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/developer/findAll")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
+        mockMvc.perform(request)
                 .andExpect(jsonPath("$", hasSize(3)))
                 .andExpect(jsonPath("$[2].name", is("Sasha")))
                 .andExpect(jsonPath("$[0].email", is("ganu@gmail.com")));
@@ -63,42 +73,22 @@ class DeveloperControllerTest {
     @Test
     public void getDeveloperById_success() throws Exception {
         when(developerService.findById(RECORD_1.getId())).thenReturn(RECORD_1);
+        JwtRequest jwtRequest = new JwtRequest();
+        jwtRequest.setIssuer("GP");
+        jwtRequest.setRoles(Collections.singleton(Role.ADMIN));
 
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/developer/find/1")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", notNullValue()))
-                .andExpect(jsonPath("$.name", is("Nikita")))
-                .andExpect(jsonPath("$.email", is("ganu@gmail.com")));
-    }
+        JwtResponse jwtResponse = authService.login(jwtRequest);
 
-    @Test
-    public void createDeveloper_success() throws Exception {
-        Developer developer = Developer.builder()
-                .id(1L)
-                .name("Andryusha")
-                .email("varlamov@gmail.com")
-                .build();
-
-
-        Map<Validation,Developer> map  = new LinkedHashMap<>();
-        map.put(Validation.CREATED,developer);
-
-        Mockito.when(developerService.save(developer)).thenReturn(map);
-
-        String content = objectWriter.writeValueAsString(developer);
-
-        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/developer/save")
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/developer/find/1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .content(content);
+                .header("Authorization", "Bearer " + jwtResponse.getAccessToken())
+                .with(SecurityMockMvcRequestPostProcessors.user("task2").roles("ADMIN"));
 
-        mockMvc.perform(mockRequest)
+        mockMvc.perform(request)
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", notNullValue()))
-                .andExpect(jsonPath("$.name", is("Andryusha")))
-                .andExpect(jsonPath("$.email", is("varlamov@gmail.com")));
+                .andExpect(jsonPath("$.name", is("Nikita")));
     }
     @Test
     public void updateDeveloper_success() throws Exception {
@@ -107,22 +97,60 @@ class DeveloperControllerTest {
                 .name("Masha")
                 .email("varlamova@gmail.com")
                 .build();
-        Map<Validation,Developer> map  = new LinkedHashMap<>();
-        map.put(Validation.CREATED,developer);
-        Mockito.when(developerService.findById(RECORD_1.getId())).thenReturn(RECORD_1);
-        Mockito.when(developerService.update(developer)).thenReturn(map);
+        JwtRequest jwtRequest = new JwtRequest();
+        jwtRequest.setIssuer("GP");
+        jwtRequest.setRoles(Collections.singleton(Role.ADMIN));
+
+        JwtResponse jwtResponse = authService.login(jwtRequest);
+        Map<Validation, Developer> map = new LinkedHashMap<>();
+        map.put(Validation.CREATED, developer);
+        when(developerService.findById(RECORD_1.getId())).thenReturn(RECORD_1);
+        when(developerService.update(developer)).thenReturn(map);
 
         String updateContent = objectWriter.writeValueAsString(developer);
 
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.put("/developer/update")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .content(updateContent);
+                .header("Authorization", "Bearer " + jwtResponse.getAccessToken())
+                .with(SecurityMockMvcRequestPostProcessors.user("task2").roles("ADMIN"));
 
-        mockMvc.perform(mockRequest)
+        mockMvc.perform(mockRequest.content(updateContent))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", notNullValue()))
                 .andExpect(jsonPath("$.name", is("Masha")))
                 .andExpect(jsonPath("$.email", is("varlamova@gmail.com")));
     }
+    @Test
+    public void createDeveloper_success() throws Exception {
+        Developer developer = Developer.builder()
+                .id(1L)
+                .name("Andryusha")
+                .email("varlamov@gmail.com")
+                .build();
+
+        JwtRequest jwtRequest = new JwtRequest();
+        jwtRequest.setIssuer("GP");
+        jwtRequest.setRoles(Collections.singleton(Role.ADMIN));
+
+        JwtResponse jwtResponse = authService.login(jwtRequest);
+        Map<Validation, Developer> map = new LinkedHashMap<>();
+        map.put(Validation.CREATED, developer);
+
+        when(developerService.save(developer)).thenReturn(map);
+        System.out.println(jwtResponse.getAccessToken());
+        String content = objectWriter.writeValueAsString(developer);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/developer/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + jwtResponse.getAccessToken());
+
+        mockMvc.perform(mockRequest.content(content))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.name", is("Andryusha")))
+                .andExpect(jsonPath("$.email", is("varlamov@gmail.com")));
+    }
+
 }
